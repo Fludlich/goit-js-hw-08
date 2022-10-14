a4bd9282b2c382981b59524a6231c8353870040f
Yurii Vynar <yurik.vinar37@gmail.com>
2022-10-14 21:19:07 +0300

    hw

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,31 @@
 
+import Vimeo from "@vimeo/player"
+import throttle from "lodash.throttle";
+
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+
+const onPlay = function(data) {
+ console.log(data)
+ localStorage.setItem('video-player-current-time', JSON.stringify(data))
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+ const currentTime = localStorage.getItem('video-player-current-time')
+
+    const lal = JSON.parse(currentTime)
+    
+    player.setCurrentTime(lal.seconds).then(function(seconds) {
+         seconds = seconds
+    }).catch(function(error) {
+        switch (error.name) {
+            case 'RangeError':
+                // the time was less than 0 or greater than the video’s duration
+                break;
+    
+            default:
+                // some other error occurred
+                break;
+        }
+    });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
 
+import throttle from "lodash.throttle";
+
+let getEl = x => document.querySelector(x)
+
+const form =getEl('.feedback-form')
+const input = getEl('input')
+const textarea = getEl('textarea')
+const STORAGE = "feedback-form-state"
+
+
+
+form.addEventListener('submit', sendUReq)
+const storage = {}
+reload()
+form.addEventListener('input', throttle(sett, 500))
+
+function sett (event){
+    storage[event.target.name]=event.target.value
+    console.log(storage)
+    localStorage.setItem(STORAGE, JSON.stringify(storage))
+}
+
+function sendUReq (event){
+    event.preventDefault();
+    event.currentTarget.reset();
+    localStorage.removeItem(STORAGE)
+}
+
+function reload (event){
+    const user = localStorage.getItem(STORAGE)
+    if(user){
+       const split =  JSON.parse(user)
+        input.value = split.email
+        textarea.value = split.message
+    }
+
+}
+console.log(storage)
