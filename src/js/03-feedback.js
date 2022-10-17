35253760294cf3d4d08b8baf86b6d3f1f716a756
Yurii Vynar <yurik.vinar37@gmail.com>
2022-10-17 17:02:22 +0300

    12

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -20,6 +20,7 @@ reload()
 form.addEventListener('input', throttle(sett, 500))
 
 function sett (event){
+    // console.log(event)
     storage[event.target.name]=event.target.value
     // console.log(storage)
     localStorage.setItem(STORAGE, JSON.stringify(storage))
@@ -27,7 +28,7 @@ function sett (event){
 
 function sendUReq (event){
     event.preventDefault();
-    // console.log(input.value.length)
+    console.log(input.value.length)
     // if(input.value.length===0){
   
     //     storage.email = ''
@@ -46,13 +47,17 @@ function sendUReq (event){
 
 function reload () {
     const user = localStorage.getItem(STORAGE)
-    if(user){
-       const split =  JSON.parse(user)
-       input.value = split.email
-       textarea.value = split.message
-        storage.email = split.email
-        storage.message = split.message
+    if (user){
+        const split =  JSON.parse(user)
+        console.log(user)
+        input.value = split.email
+        textarea.value = split.message
+         storage.email = split.email
+         storage.message = split.message
     }
+    
+      
+    
 
 }
 console.log(storage)
