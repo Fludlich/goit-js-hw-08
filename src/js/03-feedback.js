c375de5500a63f3d5f16955d3df17624919f1142
Yurii Vynar <yurik.vinar37@gmail.com>
2022-10-17 16:04:37 +0300

    de

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -7,41 +7,44 @@ const form =getEl('.feedback-form')
 const input = getEl('input')
 const textarea = getEl('textarea')
 const STORAGE = "feedback-form-state"
-
-
-
-form.addEventListener('submit', sendUReq)
 const storage = {
     email: '',
     message: '',
 }
+
+form.addEventListener('submit', sendUReq)
+
+
 reload()
+
 form.addEventListener('input', throttle(sett, 500))
 
 function sett (event){
     storage[event.target.name]=event.target.value
-    console.log(storage)
+    // console.log(storage)
     localStorage.setItem(STORAGE, JSON.stringify(storage))
 }
 
 function sendUReq (event){
     event.preventDefault();
-    console.log(input.value.length)
-    if(input.value.length===0){
+    // console.log(input.value.length)
+    // if(input.value.length===0){
   
-        storage.email = ''
+    //     storage.email = ''
        
-    }
-    if(textarea.value.length===0){
-        storage.message = ''
-    }
+    // }
+    // if(textarea.value.length===0){
+    //     storage.message = ''
+    // }
+
     console.log(storage)
     event.currentTarget.reset();
     localStorage.removeItem(STORAGE)
-  
+   storage.message = ''
+   storage.email = ''
 }
 
-function reload (event) {
+function reload () {
     const user = localStorage.getItem(STORAGE)
     if(user){
        const split =  JSON.parse(user)
